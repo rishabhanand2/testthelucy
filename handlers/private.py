49dbefe33f10b4b60e945d4e831fa0be2444f81f
Rishabh anand <90849358+rishabhanand2@users.noreply.github.com>
2021-10-22 21:09:38 +0530

    Delete private.py

--- handlers/private.py
+++
@@ -1,5 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-
-from config import BOT_NAME as bn
-from helpers.filters import 
